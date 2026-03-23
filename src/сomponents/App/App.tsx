e2b326bfaafdec269415c6d1b8ae3321a5f94c59
VitaliiSakhno <vitalik.tarasovich@gmail.com>
2026-03-23 13:12:24 +0100

    loading&error handling

--- src/сomponents/App/App.tsx
+++ src/сomponents/App/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
+
 import SearchBar from "../SearchBar/SearchBar";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 import { fetchMovies } from "../../services/movieService";
 import type { Movie } from "../../types/movie";
@@ -8,15 +11,38 @@ import "./App.module.css";
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const handleSearch = async (query: string) => {
-    const result = await fetchMovies(query);
-    setMovies(result);
+    try {
+      setIsLoading(true);
+      setIsError(false)
+      const result = await fetchMovies(query);
+      setMovies(result);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <>
       <SearchBar onSubmit={handleSearch} />
+      {movies.length > 0 && (
+        <ul>
+          {movies.map(({ id, poster_path, title }) => (
+            <li key={id}>
+              <a href={poster_path} target="_blank">
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
     </>
   );
 }
