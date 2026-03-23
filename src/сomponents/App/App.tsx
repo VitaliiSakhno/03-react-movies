28ca24731bc20e43e0b503f2c395056c923334e0
VitaliiSakhno <vitalik.tarasovich@gmail.com>
2026-03-23 12:42:28 +0100

    basic structure, components maprkup and styles&successfull http request

--- src/сomponents/App/App.tsx
+++ src/сomponents/App/App.tsx
@@ -1,14 +1,24 @@
-// import { useState } from 'react'
-import './App.module.css'
+import { useState } from "react";
+import SearchBar from "../SearchBar/SearchBar";
+
+import { fetchMovies } from "../../services/movieService";
+import type { Movie } from "../../types/movie";
+
+import "./App.module.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleSearch = async (query: string) => {
+    const result = await fetchMovies(query);
+    setMovies(result);
+  };
 
   return (
     <>
-      
+      <SearchBar onSubmit={handleSearch} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
